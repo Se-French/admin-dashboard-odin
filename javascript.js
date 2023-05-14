436fe40ebf911bdee088f893cae4f44f60dbcc82
Se-French <116462983+Se-French@users.noreply.github.com>
2023-05-14 18:29:53 -0400

    Make all links and buttons 'clickable'

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('button');
+const links = document.querySelectorAll('a');
+
+links.forEach(a => {
+    a.addEventListener('click', redirect)
+});
+
+buttons.forEach(button => {
+    button.addEventListener('click', redirect)
+});
+
+function redirect(event){
+    window.location.href = 'other_page/second.html';
+    console.log('Good job, this is working!');
+}
+
+
+
+
